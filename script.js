7c531469e0e0735f965ebb8808e3ba8235148d3e
tsiryjl <tsiry.razanakoto@gmail.com>
2021-01-13 23:31:10 +0100

    add Collapse

--- script.js
+++ script.js
@@ -21,3 +21,15 @@ function myFunction(x) {
 
 var scene = document.getElementById('js-scene');
 var parallax = new Parallax(scene);
+
+//Accordion collapse
+
+$(function ($) {
+  $(".js-accordion-title").on("click", function () {
+    /*クリックでコンテンツを開閉*/
+    $(this).next().slideToggle(200);
+    /*矢印の向きを変更*/
+    $(this).toggleClass("open", 200);
+  });
+});
+
